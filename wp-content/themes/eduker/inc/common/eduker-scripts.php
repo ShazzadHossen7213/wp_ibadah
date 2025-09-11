4ea5012fc26dd61701dc0944ab546035d8ef6e56
Shazzad Hossen <hossensajjadaitvet@gmail.com>
2025-09-11 15:31:34 +0600

    css and js link update code in

--- wp-content/themes/eduker/inc/common/eduker-scripts.php
+++ wp-content/themes/eduker/inc/common/eduker-scripts.php
@@ -19,9 +19,6 @@ function industrial_scripts() {
     wp_enqueue_style( 'meanmenu', INDUSTRIAL_THEME_CSS_DIR . 'meanmenu.css', [] );
     wp_enqueue_style( 'animate', INDUSTRIAL_THEME_CSS_DIR . 'animate.css', [] );
     wp_enqueue_style( 'owl-carousel', INDUSTRIAL_THEME_CSS_DIR . 'owl-carousel.css', [] );
-    wp_enqueue_style( 'swiper-bundle', INDUSTRIAL_THEME_CSS_DIR . 'swiper-bundle.css', [] );
-    wp_enqueue_style( 'magnific-popup', INDUSTRIAL_THEME_CSS_DIR . 'magnific-popup.css', [] );
-    wp_enqueue_style( 'nice-select', INDUSTRIAL_THEME_CSS_DIR . 'nice-select.css', [] );
     wp_enqueue_style( 'font-awesome-pro-industrial', INDUSTRIAL_THEME_CSS_DIR . 'font-awesome-pro-industrial.css', [] );
     wp_enqueue_style( 'jquery-fancybox', INDUSTRIAL_THEME_CSS_DIR . 'spacing.css', [] );
     wp_enqueue_style( 'industrial-core', INDUSTRIAL_THEME_CSS_DIR . 'industrial-core.css', [], );
@@ -31,15 +28,13 @@ function industrial_scripts() {
 
     // all js
     wp_enqueue_script( 'bootstrap-bundle', INDUSTRIAL_THEME_JS_DIR . 'bootstrap-bundle.js', [ 'jquery' ], '', true );
-    wp_enqueue_script( 'waypoints', INDUSTRIAL_THEME_JS_DIR . 'waypoints.js', [ 'jquery' ], false, true );
-    wp_enqueue_script( 'meanmenu', INDUSTRIAL_THEME_JS_DIR . 'meanmenu.js', [ 'jquery' ], false, true );
+    wp_enqueue_script( 'wow', INDUSTRIAL_THEME_JS_DIR . 'wow.min.js', [ 'jquery' ], false, true );
+    wp_enqueue_script( 'waypoints', INDUSTRIAL_THEME_JS_DIR . 'waypoints.min.js', [ 'jquery' ], false, true );
+    wp_enqueue_script( 'easing', INDUSTRIAL_THEME_JS_DIR . 'easing.min.js', [ 'jquery' ], false, true );
     wp_enqueue_script( 'counterup', INDUSTRIAL_THEME_JS_DIR . 'counterup.js', [ 'jquery' ], false, true );
-    wp_enqueue_script( 'swiper-bundle', INDUSTRIAL_THEME_JS_DIR . 'swiper-bundle.js', [ 'jquery' ], false, true );
-    wp_enqueue_script( 'owl-carousel', INDUSTRIAL_THEME_JS_DIR . 'owl-carousel.js', [ 'jquery' ], false, true );
-    wp_enqueue_script( 'magnific-popup', INDUSTRIAL_THEME_JS_DIR . 'magnific-popup.js', [ 'jquery' ], '', true );
-    wp_enqueue_script( 'parallax', INDUSTRIAL_THEME_JS_DIR . 'parallax.js', [ 'jquery' ], '', true );
-    wp_enqueue_script( 'jquery-nice-select', INDUSTRIAL_THEME_JS_DIR . 'nice-select.js', [ 'jquery' ], '', true );
-    wp_enqueue_script( 'wow', INDUSTRIAL_THEME_JS_DIR . 'wow.js', [ 'jquery' ], false, true );
+    wp_enqueue_script( 'owl-carousel', INDUSTRIAL_THEME_JS_DIR . 'owl.carousel.min.js', [ 'jquery' ], false, true );
+
+    wp_enqueue_script( 'meanmenu', INDUSTRIAL_THEME_JS_DIR . 'meanmenu.js', [ 'jquery' ], false, true );
     wp_enqueue_script( 'isotope-pkgd', INDUSTRIAL_THEME_JS_DIR . 'isotope-pkgd.js', [ 'imagesloaded' ], false, true );
     wp_enqueue_script( 'industrial-main', INDUSTRIAL_THEME_JS_DIR . 'main.js', [ 'jquery' ], false, true );
 
